2ccd2e13bcba1467d28c9c176a3e6e5158809392
Steve Korzinetzki <steve.korzinetzki@empowersuite.com>
2021-12-13 19:42:51 +0100

    initial solution setup

---
+++ csharp-1/de.swk.aoc-2021/de.swk.aoc-2021/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace de.swk.aoc_2021
+{
+    public class Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
